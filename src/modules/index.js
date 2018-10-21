562f6995b91b07f82b4a7e639c49e72609a8b8ae
Jeff Loyd <jeffloyd@gmail.com>
2018-10-21 14:51:25 -0500

    Changed Endpoint Message on JWT secured area

--- src/modules/index.js
+++ src/modules/index.js
@@ -6,7 +6,7 @@ import { authJwt } from "../services/auth.services";
  export default app => {
      app.use('/api/v1/users', userRoutes);
      app.use('/api/v1/resources', resourceRoutes);
-     app.get('/hello', authJwt, (req,res)=>{
-         res.send('this is private!');
+     app.get('/secured', authJwt, (req,res)=>{
+         res.send('JWT Protected Example');
      })
  };
